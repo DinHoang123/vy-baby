9148244a9c94bd437005e5438d68e73d682bcf4a
DinHoang123 <106921833+DinHoang123@users.noreply.github.com>
2022-06-06 21:34:00 +0700

    first commit

---
+++ YTB-youtube-2021-totinh-part-2-master/config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hé lu ! 🖐🖐🖐',
+    introDesc: `In thís page , i want to talk about you!!!! you want to listen`,
+    btnIntro: 'OK',
+    title: 'I am really really like you ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
+    btnYes: 'Có, em thật sự thích anh 💖💖💖',
+    btnNo: '💔 No, i am sory ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'vì anh đẹp trai ( hoặc không ) 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 3000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/dong.hoang.35912672'
+}
